73d1a43e8b4b35815fd6354084eee866feb5a788
Joel Christner <joelchristner@Joels-MacBook-Pro-2.local>
2017-12-18 15:41:22 -0800

    Copyright and license

--- CloudGenixSdk/Classes/Common.cs
+++ CloudGenixSdk/Classes/Common.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+
+    CloudGenix Controller SDK
+    (c) 2017 CloudGenix, Inc.
+    All Rights Reserved
+
+    https://www.cloudgenix.com
+
+    This SDK is released under the MIT license.
+    For support, please contact us on:
+
+        NetworkToCode Slack channel #cloudgenix: http://slack.networktocode.com
+        Email: developers@cloudgenix.com
+
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;

9ce74656b26d86e1e2b1b098f627d1fa44416c6a
Joel Christner <joel@cloudgenix.com>
2017-12-19 14:27:23 -0800

    NuGet v1.0.0

--- CloudGenixSdk/Properties/AssemblyInfo.cs
+++ CloudGenixSdk/Properties/AssemblyInfo.cs
@@ -1,23 +1,26 @@
-﻿using System.Reflection;
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("CloudGenixSdk")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CloudGenix Controller SDK")]
+[assembly: AssemblyDescription("SDK for the CloudGenix Controller")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CloudGenix, Inc")]
+[assembly: AssemblyProduct("CloudGenix Controller SDK")]
+[assembly: AssemblyCopyright("(c)2017 CloudGenix, Inc")]
+[assembly: AssemblyTrademark("(TM) CloudGenix, Inc.")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("00000000-0000-0000-0000-000000000000")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- SdkCli/Properties/AssemblyInfo.cs
+++ SdkCli/Properties/AssemblyInfo.cs
@@ -1,23 +1,26 @@
-﻿using System.Reflection;
+﻿using System.Runtime.InteropServices;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("SdkCli")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Test Application for CloudGenix Controller SDK")]
+[assembly: AssemblyDescription("Test Application for CloudGenix Controller SDK")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("CloudGenix, Inc.")]
+[assembly: AssemblyProduct("Test Application for CloudGenix Controller SDK")]
+[assembly: AssemblyCopyright("(c)2017 CloudGenix, Inc.")]
+[assembly: AssemblyTrademark("(TM) CloudGenix, Inc.")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("00000000-0000-0000-0000-000000000000")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.

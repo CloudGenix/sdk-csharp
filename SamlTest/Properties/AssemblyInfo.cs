665b90368f8dee62a9b9af777221b6cb4de83778
Joel Christner <joel@cloudgenix.com>
2018-03-12 14:18:52 -0700

    NuGet v1.0.3, release build with SAML

--- SamlTest/Properties/AssemblyInfo.cs
+++ SamlTest/Properties/AssemblyInfo.cs
@@ -7,17 +7,18 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("SamlTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CloudGenix, Inc.")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("${AuthorCopyright}")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("(c) 2018 CloudGenix, Inc.")]
+[assembly: AssemblyTrademark("(TM) CloudGenix, Inc.")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- SdkCli/Properties/AssemblyInfo.cs
+++ SdkCli/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CloudGenix, Inc.")]
 [assembly: AssemblyProduct("Test Application for CloudGenix Controller SDK")]
-[assembly: AssemblyCopyright("(c)2017 CloudGenix, Inc.")]
+[assembly: AssemblyCopyright("(c)2018 CloudGenix, Inc.")]
 [assembly: AssemblyTrademark("(TM) CloudGenix, Inc.")]
 [assembly: AssemblyCulture("")]
 
@@ -18,8 +18,8 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
 [assembly: Guid("00000000-0000-0000-0000-000000000000")]
 
 // The following attributes are used to specify the signing key for the assembly, 
